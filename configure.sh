5b200ec019c06890f086a16a3c0e1117432250c9
Dmitry Koplyarov <koplyarov.da@gmail.com>
2015-06-27 13:05:16 +0300

    Redirected first cmake call stdout and stderr to /dev/null

--- configure.sh
+++ configure.sh
@@ -72,5 +72,5 @@ BuildArtifactsToDelete="CMakeCache.txt CMakeFiles Makefile
 
 rm -rf ${BuildArtifactsToDelete}
 
-cmake -DTOOLCHAIN_PATH:FILEPATH="${EdisonSdkInstallPath}" -DTOOLCHAIN_FILE="${RootDirectory}/edison.cmake" "${RootDirectory}"
+cmake -DTOOLCHAIN_PATH:FILEPATH="${EdisonSdkInstallPath}" -DTOOLCHAIN_FILE="${RootDirectory}/edison.cmake" "${RootDirectory}" >/dev/null 2>/dev/null
 cmake -DTOOLCHAIN_PATH:FILEPATH="${EdisonSdkInstallPath}" -DTOOLCHAIN_FILE="${RootDirectory}/edison.cmake" "${RootDirectory}"
