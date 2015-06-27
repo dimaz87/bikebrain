2437f0044653a8c6407176637d2da789bb88ffa9
Dmitry Koplyarov <koplyarov.da@gmail.com>
2015-06-27 10:24:50 +0300

    First source code files

---
+++ bikebrain/ILedMatrix.h
@@ -0,0 +1,16 @@
+#ifndef BIKEBRAIN_ILEDMATRIX_H
+#define BIKEBRAIN_ILEDMATRIX_H
+
+namespace bikebrain
+{
+
+	struct ILedMatrix
+	{
+		virtual ~ILedMatrix() { }
+
+		/* methods */
+	};
+
+}
+
+#endif
---
+++ bikebrain/platform/emu/EmuLedMatrix.h
@@ -0,0 +1,16 @@
+#ifndef BIKEBRAIN_PLATFORM_EMU_EMULEDMATRIX_H
+#define BIKEBRAIN_PLATFORM_EMU_EMULEDMATRIX_H
+
+#include <bikebrain/ILedMatrix.h>
+
+namespace bikebrain {
+namespace emu
+{
+
+	class EmuLedMatrix : public virtul ILedMatrix
+	{
+	};
+
+}}
+
+#endif
